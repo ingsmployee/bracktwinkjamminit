724945b3c87262d2677bcd9a0a4eb5c98c5784fc
nimeemaw <mediocrealarm72@gmail.com>
2025-02-18 17:00:06 -0700

    i uhh

--- dogsbuilddogsbuilddogs/Assets/Code/building.gd
+++ dogsbuilddogsbuilddogs/Assets/Code/building.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-var mouse_hovered: bool = false
-var highlight: bool = false
+var highlighted: bool = false
+var highlightCopy: Sprite2D
 var buildings_touched: Array[Node2D]
 var prox_bonus_amount: Array[float] = [0,0,0]
 
 var progress: float = 0
+var destination: float = 100
 
 var placed: bool = false
 @export_enum("Fun", "Safe", "Industry") var prox_bonus_type: int = 0
@@ -18,27 +19,50 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if prox_bonus_type == 2:
-		progress += delta
-	for i in range(floor(progress / 100)):
-		progress -= 100
-		produce()
+	var multiplier = 1
+	if placed:
+		if prox_bonus_type == 2:
+			progress += delta * multiplier
+		if progress > destination:
+			var productions:int = floor(progress / destination)
+			produce(productions)
+			progress -= productions * destination
 
-func produce() -> void:
-	pass ## put stuff here
+func produce(amount: int) -> void:
+	print("Producing!")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("mouse_primary") && mouse_hovered && get_parent().get_parent().placement == null:
-		var tween = get_tree().create_tween()
-		tween.tween_property($"highlight areas", "modulate", Color(1,1,1,1), 0.3)
-		$Sprite2D.z_index = 3
+	if event.is_action_pressed("mouse_primary") && get_parent().get_parent().placement == null:
+		hide_circles()
+
+func _on_area_2d_building_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("mouse_primary") && get_parent().get_parent().placement == null:
+		show_circles()
+		highlight()
 		
 		# for building in buildings_touched:
 			# building.get_node("Sprite2D").self_modulate = Color(0.5,1,0.5,1)
 		print(prox_bonus_amount)
 
-func _on_area_2d_building_mouse_entered() -> void:
-	mouse_hovered = true
+func show_circles() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($"highlight areas", "modulate", Color(1,1,1,1), 0.3)
+	
+	# for building in buildings_touched:
+		# building.get_node("Sprite2D").self_modulate = Color(0.5,1,0.5,1)
+	print(prox_bonus_amount)
+
+func hide_circles() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($"highlight areas", "modulate", Color(1,1,1,0), 0.3)
+
+func highlight() -> void:
+	$Sprite2D.z_index = 3
+	highlighted = true
+
+func unhighlight() -> void:
+	$Sprite2D.z_index = 1
+	highlighted = false
 
 func place() -> void:
 	$Sprite2D.self_modulate = Color(1,1,1,1)
@@ -53,7 +77,6 @@ func place() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($"highlight areas", "modulate", Color(1,1,1,0), 0.3)
 	$Sprite2D.z_index = 1
-	mouse_hovered = false
 
 func remove() -> void:
 	var tween = get_tree().create_tween()
@@ -62,13 +85,6 @@ func remove() -> void:
 	if placed:
 		for building in buildings_touched:
 			building.prox_bonus_amount[prox_bonus_type] -= prox_bonus_addition
-
-func _on_area_2d_building_mouse_exited() -> void:
-	if placed:
-		var tween = get_tree().create_tween()
-		tween.tween_property($"highlight areas", "modulate", Color(1,1,1,0), 0.3)
-		$Sprite2D.z_index = 1
-		mouse_hovered = false
 
 func _on_proximity_bonus_area_2d_area_entered(area: Area2D) -> void:
 	area.get_parent().get_node("Sprite2D").self_modulate = Color(0.5,1,0.5,1)
